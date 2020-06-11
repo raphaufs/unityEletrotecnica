d7bbed8d5840ffe4255f7f82e0aa25b23cc1ee26
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-06-11 14:43:42 -0300

    feat: add switch scene menu, refactor name scene

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+    private GameObject objAtual;
+    private string nameObjeto;
+    void Start()
+    {
+        this.objAtual = this.gameObject;
+        this.nameObjeto = objAtual.name;
+    }
+
+   public void switchScene()
+    {
+        SceneManager.LoadScene(this.nameObjeto);
+    }
+
+    public void backForStation() 
+    {
+        SceneManager.LoadScene("cena1");
+        // SceneManager.LoadScene("cena1");
+    }
+}
