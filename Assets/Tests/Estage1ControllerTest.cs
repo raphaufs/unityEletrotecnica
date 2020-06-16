34049e1e70abca65435ba3155c733de763a87324
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-06-16 16:58:19 -0300

    chore: add test unit and indexReferences

---
+++ Assets/Tests/Estage1ControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Estage1ControllerTest
+    {
+        [Test]
+        public void manobraDesenergizarTest()
+        {
+            // Should return true if all components required is true
+
+        }
+    }
+}
