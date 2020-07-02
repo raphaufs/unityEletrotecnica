ed0974c8fb577359e5cb8fc7d954ad41742ce0c9
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-07-02 16:21:54 -0300

    feat: cutscene

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    public List<GameObject> cutScenes = new List<GameObject>();
+    public Camera main;
+
+    public void startCutscene(int id)
+    {
+        main.gameObject.SetActive(false);
+        cutScenes[id].SetActive(true);
+    }
+
+    public void StopCutscene(int id)
+    {
+        main.gameObject.SetActive(true);
+        cutScenes[id].SetActive(false); 
+    }
+}
